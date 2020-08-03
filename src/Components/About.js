0d016c81652c9a8cdcea0bffb1305f852272267c
Anna <nuleeannajeon@gmail.com>
2020-08-03 01:41:47 -0400

    created react portfolio

---
+++ src/Components/About.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var profilepic= "images/"+this.props.data.image;
+      var bio = this.props.data.bio;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+      var resumeDownload = this.props.data.resumedownload;
+    }
+
+    return (
+      <section id="about">
+      <div className="row" style={{textAlign: "center"}}>
+         <div className="twelve columns">
+            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+         </div>
+         <div className="twelve columns">
+            <h2>Who's Anna?</h2>
+
+            <p>{bio}</p>
+            <div className="row">
+               <div className="columns contact-details">
+                  <h2>Contact Details</h2>
+                  <p className="address">
+						   <span>{name}</span><br />
+						   <span>{street}<br />
+						         {city} {state}, {zip}
+                   </span><br />
+						   <span>{phone}</span><br />
+                     <span>{email}</span>
+					   </p>
+               </div>
+               <div className="columns download">
+                  <p>
+                  {/* <a className="button" href="http://localhost/assets/Resume.pdf" download="AnnaJeon.pdf"><i className="fa fa-download"></i>Download Resume</a> */}
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+
+   </section>
+    );
+  }
+}
+
+export default About;
